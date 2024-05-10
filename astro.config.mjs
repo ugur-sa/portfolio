85da160db3220526e8942429302b6d764d956510
Ugur Sadiklar <sadiklarugur@gmail.com>
2024-05-11 01:33:18 +0200

    Fix vercel analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,7 +10,9 @@ import vercel from '@astrojs/vercel/serverless';
 export default defineConfig({
 	output: 'server',
 	adapter: vercel({
-		webAnalytics: { enabled: true }
+		webAnalytics: {
+			enabled: true
+		}
 	}),
 	integrations: [tailwind(), mdx(), react()]
 });
