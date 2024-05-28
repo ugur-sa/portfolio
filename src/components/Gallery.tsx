45d6c85bf60d6d70d66b4ab0bdb86c9cc4ad619c
ugur-sa <sadiklarugur@gmail.com>
2024-05-28 15:22:44 +0200

    Change chevron icons

--- src/components/Gallery.tsx
+++ src/components/Gallery.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { BsChevronCompactLeft, BsChevronCompactRight } from 'react-icons/bs';
+import { MdChevronLeft, MdChevronRight } from 'react-icons/md';
 import { RxDotFilled } from 'react-icons/rx';
 import { IoClose } from 'react-icons/io5';
 
@@ -35,27 +35,29 @@ const Gallery: React.FC<GalleryProps> = ({ images }) => {
 	};
 
 	return (
-		<div className="group relative m-auto h-[450px] w-full overflow-hidden rounded-lg border border-zinc-600 md:max-h-[500px] lg:max-h-[500px]">
-			<div
-				className="absolute -z-50 h-full w-full overflow-hidden object-cover opacity-30 blur-xl"
-				style={{
-					backgroundImage: `url(${images[currentImage]})`
-				}}
-			></div>
-			<div
-				className="h-full w-full cursor-zoom-in rounded-2xl bg-contain bg-center bg-no-repeat duration-500"
-				style={{ backgroundImage: `url(${images[currentImage]})` }}
-				onClick={openModal}
-			></div>
-			{/* Left Arrow*/}
-			<div className="absolute left-5 top-[50%] hidden -translate-x-0 translate-y-[-50%] cursor-pointer rounded-full bg-black/20 p-2 text-2xl text-white hover:bg-black/50 group-hover:block">
-				<BsChevronCompactLeft size={30} onClick={previousSlide} />
+		<div className="group m-auto h-[450px] w-full rounded-lg border border-zinc-600 md:max-h-[500px] lg:max-h-[500px]">
+			<div className="relative h-full overflow-hidden">
+				<div
+					className="absolute -z-50 h-full w-full opacity-60 blur-xl"
+					style={{
+						backgroundImage: `url(${images[currentImage]})`
+					}}
+				/>
+				<div
+					className="h-full w-full cursor-zoom-in rounded-2xl bg-contain bg-center bg-no-repeat duration-500"
+					style={{ backgroundImage: `url(${images[currentImage]})` }}
+					onClick={openModal}
+				></div>
+				{/* Left Arrow*/}
+				<div className="absolute left-5 top-[50%] hidden -translate-x-0 translate-y-[-50%] cursor-pointer rounded-full bg-black/20 p-1 text-2xl text-white hover:bg-black/50 group-hover:block">
+					<MdChevronLeft size={30} onClick={previousSlide} />
+				</div>
+				{/* Right Arrow*/}
+				<div className="absolute right-5 top-[50%] hidden -translate-x-0 translate-y-[-50%] cursor-pointer rounded-full bg-black/20 p-1 text-2xl text-white hover:bg-black/50 group-hover:block">
+					<MdChevronRight size={30} onClick={nextSlide} />
+				</div>
 			</div>
-			{/* Right Arrow*/}
-			<div className="absolute right-5 top-[50%] hidden -translate-x-0 translate-y-[-50%] cursor-pointer rounded-full bg-black/20 p-2 text-2xl text-white hover:bg-black/50 group-hover:block">
-				<BsChevronCompactRight size={30} onClick={nextSlide} />
-			</div>
-			<div className="top-4 flex justify-center py-2">
+			<div className="m-auto mt-2 flex w-fit rounded-full bg-white/20">
 				{images.map((slide, slideIndex) => (
 					<div
 						key={slideIndex}
@@ -78,18 +80,18 @@ const Gallery: React.FC<GalleryProps> = ({ images }) => {
 							/>
 							<button
 								onClick={closeModal}
-								className="absolute right-0 top-0 z-50 mr-2 mt-2 rounded-full bg-black/20 p-2"
+								className="absolute right-0 top-0 z-50 mr-2 mt-2 rounded-full bg-black/30 p-2"
 							>
-								<IoClose size={40} />
+								<IoClose size={30} />
 							</button>
 							<img src={images[currentImage]} alt="Modal" />
 							{/* Left Arrow*/}
-							<div className="absolute left-5 top-[50%] hidden -translate-x-0 translate-y-[-50%] cursor-pointer rounded-full bg-black/20 p-2 text-2xl text-white group-hover:block">
-								<BsChevronCompactLeft size={30} onClick={previousSlide} />
+							<div className="absolute left-5 top-[50%] hidden -translate-x-0 translate-y-[-50%] cursor-pointer rounded-full bg-black/20 p-1 text-2xl text-white group-hover:block">
+								<MdChevronLeft size={30} onClick={previousSlide} />
 							</div>
 							{/* Right Arrow*/}
-							<div className="absolute right-5 top-[50%] hidden -translate-x-0 translate-y-[-50%] cursor-pointer rounded-full bg-black/20 p-2 text-2xl text-white group-hover:block">
-								<BsChevronCompactRight size={30} onClick={nextSlide} />
+							<div className="absolute right-5 top-[50%] hidden -translate-x-0 translate-y-[-50%] cursor-pointer rounded-full bg-black/20 p-1 text-2xl text-white group-hover:block">
+								<MdChevronRight size={30} onClick={nextSlide} />
 							</div>
 							<div className="absolute bottom-10 flex justify-center rounded-full bg-black/20">
 								{images.map((slide, slideIndex) => (
